82be3e2f8d7fdbf0d0733707cb34e4ae541b9b19
Weiwei Zhu <zhuwwpku@gmail.com>
2018-01-18 00:31:02 +0800

    being tested.

--- copyfile.py
+++ copyfile.py
@@ -7,11 +7,15 @@ container = "presto"
 
 
 
-#print("docker cp ~/local/src/presto-docker/DDplan.py %s:/home/psr/software/presto/bin/"%container)
-#os.system("docker cp ~/local/src/presto-docker/DDplan.py %s:/home/psr/software/presto/bin/"%container)
+print("docker cp ~/local/src/presto-docker/DDplan.py %s:/home/psr/software/presto/bin/"%container)
+os.system("docker cp ~/local/src/presto-docker/DDplan.py %s:/home/psr/software/presto/bin/"%container)
 print("docker cp ~/local/src/presto-docker/fastpipe %s:/home/psr/software/presto/bin/"%container)
 os.system("docker cp ~/local/src/presto-docker/fastpipe %s:/home/psr/software/presto/bin/"%container)
-#print("docker cp ~/local/src/presto-docker/makezap.py %s:/home/psr/software/presto/bin/"%container)
-#os.system("docker cp ~/local/src/presto-docker/makezap.py %s:/home/psr/software/presto/bin/"%container)
-#print("docker cp ~/local/src/presto-docker/stopall %s:/usr/local/bin/"%container)
-#os.system("docker cp ~/local/src/presto-docker/stopall %s:/usr/local/bin/"%container)
+print("docker cp ~/local/src/presto-docker/makezap.py %s:/home/psr/software/presto/bin/"%container)
+os.system("docker cp ~/local/src/presto-docker/makezap.py %s:/home/psr/software/presto/bin/"%container)
+print("docker cp ~/local/src/presto-docker/stopall %s:/usr/local/bin/"%container)
+os.system("docker cp ~/local/src/presto-docker/stopall %s:/usr/local/bin/"%container)
+print("docker commit %s"%container)
+os.system("docker commit %s"%container)
+
+
