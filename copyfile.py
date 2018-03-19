5dd59dd567af0e5fa84c1015037b3ca5b5e5fe99
Weiwei Zhu <zhuwwpku@gmail.com>
2018-03-19 20:01:25 +0800

    commit corrent working state. - 2018/03/19

--- copyfile.py
+++ copyfile.py
@@ -7,16 +7,16 @@ container = "presto"
 
 
 
-#print("docker cp ~/local/src/presto-docker/DDplan.py %s:/home/psr/software/presto/bin/"%container)
-#os.system("docker cp ~/local/src/presto-docker/DDplan.py %s:/home/psr/software/presto/bin/"%container)
-#print("docker cp ~/local/src/presto-docker/fastpipe %s:/home/psr/software/presto/bin/"%container)
-#os.system("docker cp ~/local/src/presto-docker/fastpipe %s:/home/psr/software/presto/bin/"%container)
+print("docker cp ~/local/src/presto-docker/DDplan.py %s:/home/psr/software/presto/bin/"%container)
+os.system("docker cp ~/local/src/presto-docker/DDplan.py %s:/home/psr/software/presto/bin/"%container)
+print("docker cp ~/local/src/presto-docker/fastpipe %s:/home/psr/software/presto/bin/"%container)
+os.system("docker cp ~/local/src/presto-docker/fastpipe %s:/home/psr/software/presto/bin/"%container)
 print("docker cp ~/local/src/presto-docker/sifting.py %s:/home/psr/software/presto/lib/python/"%container)
 os.system("docker cp ~/local/src/presto-docker/sifting.py %s:/home/psr/software/presto/lib/python/"%container)
-#print("docker cp ~/local/src/presto-docker/makezap.py %s:/home/psr/software/presto/bin/"%container)
-#os.system("docker cp ~/local/src/presto-docker/makezap.py %s:/home/psr/software/presto/bin/"%container)
-#print("docker cp ~/local/src/presto-docker/stopall %s:/usr/local/bin/"%container)
-#os.system("docker cp ~/local/src/presto-docker/stopall %s:/usr/local/bin/"%container)
+print("docker cp ~/local/src/presto-docker/makezap.py %s:/home/psr/software/presto/bin/"%container)
+os.system("docker cp ~/local/src/presto-docker/makezap.py %s:/home/psr/software/presto/bin/"%container)
+print("docker cp ~/local/src/presto-docker/stopall %s:/usr/local/bin/"%container)
+os.system("docker cp ~/local/src/presto-docker/stopall %s:/usr/local/bin/"%container)
 print("docker commit %s"%container)
 os.system("docker commit %s"%container)
 
--- stopallnodes.sh
+++ stopallnodes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in {2..15}; do  ssh node$i ~/local/src/presto-docker/stoppipe; done
+for i in {2..15}; do echo node$i;  ssh node$i ~/local/src/presto-docker/stoppipe; done
